0fe7d67c37328e1c59e4dff2e06b004738a78f5a
ChromaMaster <vladtel.dev@gmail.com>
2021-02-28 17:09:37 +0100

    feat: Improve network to reconnect automatically

--- components/moth-network/moth-network.c
+++ components/moth-network/moth-network.c
@@ -6,39 +6,59 @@
 #include "esp_system.h"
 #include "esp_wifi.h"
 
-#include "component_interface.h"
+#include "moth-comm.h"
 
-#define ESP_WIFI_SSID ""
-#define ESP_WIFI_PASS ""
-#define EXAMPLE_ESP_MAXIMUM_RETRY 5
+#define ESP_WIFI_SSID "iot-node"
+#define ESP_WIFI_PASS "goodlife"
+//#define ESP_WIFI_SSID "Mitm"
+//#define ESP_WIFI_PASS "password"
+#define ESP_MAXIMUM_RETRY 5
 
 static shared_info_t *shared_info = NULL;
+static EventGroupHandle_t s_wifi_event_group;
 
 static const char *TAG = "moth-network";
 
 static int s_retry_num = 0;
 
-static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-        esp_wifi_connect();
-    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
-            esp_wifi_connect();
-            s_retry_num++;
-            ESP_LOGI(TAG, "retry to connect to the AP");
-        } else {
-            xEventGroupSetBits(shared_info->event_group, WIFI_FAIL_BIT);
+static void wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+    switch (event_id) {
+        case WIFI_EVENT_STA_START:
+            break;
+        case WIFI_EVENT_STA_CONNECTED:
+            ESP_LOGI(TAG, "WiFi event: WIFI_EVENT_STA_CONNECTED");
+            moth_comm_set_network_status(NETWORK_CONNECTED);
+            xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+            s_retry_num = 0;
+            break;
+        case WIFI_EVENT_STA_DISCONNECTED:
+            moth_comm_set_network_status(NETWORK_DISCONNECTED);
+            xEventGroupClearBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+            ESP_LOGI(TAG, "WiFi event: WIFI_EVENT_STA_DISCONNECTED");
+            if (s_retry_num < ESP_MAXIMUM_RETRY) {
+                ESP_LOGI(TAG, "Retry to connect to the AP");
+                esp_wifi_connect();
+                s_retry_num++;
+            } else {
+                xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+            }
+            break;
+        case IP_EVENT_STA_GOT_IP: {
+            ESP_LOGI(TAG, "IP event: IP_EVENT_STA_GOT_IP");
+            ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+            ESP_LOGI(TAG, "Got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+            break;
         }
-        ESP_LOGI(TAG, "connect to the AP fail");
-    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
-        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
-        s_retry_num = 0;
-        xEventGroupSetBits(shared_info->event_group, WIFI_CONNECTED_BIT);
+        case IP_EVENT_STA_LOST_IP:
+            break;
+        default:
+            break;
     }
 }
 
 void wifi_init_sta(void) {
+    s_wifi_event_group = xEventGroupCreate();
+
     ESP_ERROR_CHECK(esp_netif_init());
 
     ESP_ERROR_CHECK(esp_event_loop_create_default());
@@ -47,12 +67,7 @@ void wifi_init_sta(void) {
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-    esp_event_handler_instance_t instance_any_id;
-    esp_event_handler_instance_t instance_got_ip;
-    ESP_ERROR_CHECK(
-        esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, &instance_any_id));
-    ESP_ERROR_CHECK(
-        esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, &instance_got_ip));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(NULL, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL, NULL));
 
     wifi_config_t wifi_config = {
         .sta =
@@ -67,26 +82,31 @@ void wifi_init_sta(void) {
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
     ESP_ERROR_CHECK(esp_wifi_start());
 
-    ESP_LOGI(TAG, "wifi_init_sta finished.");
+    ESP_LOGI(TAG, "WiFi STA initialized");
+}
+
+uint8_t wifi_connect(void) {
+    ESP_LOGI(TAG, "Connecting to AP SSID:%s password:%s", ESP_WIFI_SSID, ESP_WIFI_PASS);
+    s_retry_num = 0;
+    esp_wifi_connect();
 
     /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
-     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
-    EventBits_t bits = xEventGroupWaitBits(shared_info->event_group, WIFI_CONNECTED_BIT | WIFI_FAIL_BIT, pdFALSE,
-                                           pdFALSE, portMAX_DELAY);
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() */
+    EventBits_t bits =
+        xEventGroupWaitBits(s_wifi_event_group, WIFI_CONNECTED_BIT | WIFI_FAIL_BIT, pdFALSE, pdFALSE, portMAX_DELAY);
 
     /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
      * happened. */
     if (bits & WIFI_CONNECTED_BIT) {
-        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s", ESP_WIFI_SSID, ESP_WIFI_PASS);
+        ESP_LOGI(TAG, "Connected");
     } else if (bits & WIFI_FAIL_BIT) {
-        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s", ESP_WIFI_SSID, ESP_WIFI_PASS);
+        ESP_LOGI(TAG, "Failed to connect");
+        return 1;
     } else {
         ESP_LOGE(TAG, "UNEXPECTED EVENT");
     }
 
-    /* The event will not be processed after unregister */
-    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
-    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
+    return 0;
 }
 
 _Noreturn void moth_network_task(void *pvParameters) {
@@ -97,9 +117,19 @@ _Noreturn void moth_network_task(void *pvParameters) {
 
     ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
     wifi_init_sta();
+    wifi_connect();
 
-    const TickType_t xDelay = 1000 / portTICK_PERIOD_MS;
+    const TickType_t period = 1000 / portTICK_PERIOD_MS;
+    const TickType_t reconnect_delay = 10000 / portTICK_PERIOD_MS;
     while (1) {
-        vTaskDelay(xDelay);
+        EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group, WIFI_CONNECTED_BIT, pdFALSE, pdFALSE, 0);
+        if ((bits & WIFI_CONNECTED_BIT) == 0) {
+            // Cannot connect, wait a bit longer and try again.
+            if (wifi_connect() != 0) {
+                vTaskDelay(reconnect_delay);
+            }
+        }
+
+        vTaskDelay(period);
     }
 }
