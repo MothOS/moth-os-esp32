858bc63553e387ba38b8ef36db8ac779306ebc61
ChromaMaster <vladtel.dev@gmail.com>
2021-03-21 16:51:05 +0100

    chore: Update built script

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 SDK_VER=$1
 shift
 
-excom="docker run --rm -it -v $PWD:/project -w /project -e UID="`id -u`" -e GID="`id -g`" --device=/dev/ttyUSB0 --entrypoint ./scripts/entry.sh espressif/idf:v${SDK_VER} $@"
+USB_DEVICES=""
+for device in $(ls /dev/ | grep "ttyUSB" | xargs -L1); do
+  USB_DEVICES="$USB_DEVICES --device=/dev/$device"
+done
+
+excom="docker run --rm -it -v $PWD:/project -w /project -e UID="`id -u`" -e GID="`id -g`" $USB_DEVICES chromamaster/esp-idf-builder:v${SDK_VER} $@"
 echo $excom
 exec $excom
