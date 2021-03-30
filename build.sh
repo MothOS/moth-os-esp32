45eaa6ae5d4359fc87666354b904ccca74dc5936
Vlad Teletin <7367209+ChromaMaster@users.noreply.github.com>
2021-03-30 18:30:46 +0200

    Update build script to get correctly the project dir

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+PROJECT_DIR="$(realpath $0 | xargs dirname)"
 SDK_VER=$1
 shift
 
@@ -8,6 +9,6 @@ for device in $(ls /dev/ | grep "ttyUSB" | xargs -L1); do
   USB_DEVICES="$USB_DEVICES --device=/dev/$device"
 done
 
-excom="docker run --rm -it -v $PWD:$PWD -w $PWD -e UID="`id -u`" -e GID="`id -g`" $USB_DEVICES chromamaster/esp-idf-builder:v${SDK_VER} $@"
+excom="docker run --rm -it -v $PROJECT_DIR:$PROJECT_DIR -w $PWD -e UID="`id -u`" -e GID="`id -g`" $USB_DEVICES chromamaster/esp-idf-builder:v${SDK_VER} $@"
 echo $excom
 exec $excom
